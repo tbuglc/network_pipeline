b1963c0305775f6bb3f0fcd4cd93f951b07bd5ed
tbuglc <tbuglc@gmail.com>
2023-03-01 22:15:30 -0500

    refactoring

--- cli/pipeline.py
+++
@@ -1,9 +0,0 @@
-import inquirer
-
-# Define the list of questions to ask the user
-questions = [
-    inquirer.Text('name', message="What's your name?"),
-    inquirer.List('gender', message="What's your gender?", choices=['Male', 'Female', 'Other']),
-    inquirer.Checkbox('languages', message="What programming languages do you know?",
-                      choices=['Python', 'Java', 'JavaScript', 'C++'])
-]
