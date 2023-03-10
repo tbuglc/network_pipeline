09ae1190f207e44f8197e8284d90c25710b6f138
tbuglc <tbuglc@gmail.com>
2023-03-10 16:00:32 -0500

    latest modification with an example file

--- graph_filter/utils.py
+++ graph_filter/utils.py
@@ -55,6 +55,23 @@ def create_folder_if_not_exist(path):
     dir_exits = os.path.exists(path)
     if not dir_exits:
         os.mkdir(Path(path))
+
+def parse_output_dir(path):
+    if path == '':
+        raise 'Path is required'
+    file_name = ''
+    if '/' in path:
+        file_name = path.split('/')[-1]
+    if '\\' in path:
+        file_name = path.split('\\')[-1]
+    
+    if '.' not in file_name:
+        raise ValueError('file missing extension')
+    
+    output_dir = path.split(file_name)[0]
+
+    return output_dir, file_name
+  
 '''
 # FIXME: END OF DUPLICATE
 '''
--- graph_metrics/main.py
+++ graph_metrics/main.py
@@ -3,35 +3,34 @@ from utils import get_start_and_end_date,  add_sheet_to_xlsx, create_xlsx_file, 
 from graph_loader import load_accorderie_network
 from datetime import date
 from snapshot_generator import create_snapshots
-from metrics import compute_global_properties_on_graph, compute_graph_metrics
+from metrics import compute_global_properties_on_graph, compute_graph_metrics, compute_average_metrics
 import argparse
 from pathlib import Path
+from utils import create_folder_if_not_exist, parse_output_dir
+import pandas as pd
 
-
-
-def main(span_days, folder_name, input_dir, output_dir, g=None):
-    folder_name = folder_name + '/'
+def main(span_days, input_dir, output_dir, g=None):
 
     start_date, end_date = get_start_and_end_date(input_dir=input_dir)
 
     if isinstance(start_date, date) == False or isinstance(end_date, date) == False or span_days < 1:
         raise "Incorrect date(s)"
 
-    if folder_name == '' or folder_name is None:
+    if output_dir == '' or output_dir is None:
         raise "Missing folder name"
 
     if g is None:
         g = load_accorderie_network(input_dir=input_dir)
 
-    folder_name = output_dir + folder_name + '/'
-
-    # FIXME: make use of util function in graph_common
-    # TODO: refactor utils in graph_metrics
-    dir_exits = os.path.exists(folder_name)
-    if not dir_exits:
-        os.mkdir(Path(folder_name))
-
-    file_writer = create_xlsx_file(folder_name + '/graphs_metrics')
+   
+    dest_dir, file_name = parse_output_dir(output_dir)
+ 
+    create_folder_if_not_exist(dest_dir)
+    
+    if file_name.endswith('.xlsx'):
+        raise ValueError('Output dir should end with file.xlsx')
+    
+    file_writer = create_xlsx_file(output_dir)
 
     global_metrics = compute_global_properties_on_graph(g=g)
     
@@ -39,22 +38,46 @@ def main(span_days, folder_name, input_dir, output_dir, g=None):
                       data=global_metrics, title='Global Metrics', index=True)
     print('Computed global metrics')
     # compute snapshots
-
-    print(g)
+    print('Creating snapshots')
     snapshots = create_snapshots(
         g, start_date=start_date, end_date=end_date, span_days=int(span_days))
-    print('Created snapshots')
+    print('Snapshots created')
+
     if len(snapshots) == 0:
         print('Could not create snapshots')
         return
 
+    # for snapshot in snapshots:
+    #     sub_graph = snapshot['subgraph']
+
+    #     metrics_row = compute_graph_metrics(g=sub_graph)
+
+    #     add_sheet_to_xlsx(file_writer=file_writer,
+    #                       data=metrics_row, title='from ' + snapshot['title'])
+    
+    average_metrics = []
+    indices = []
     for snapshot in snapshots:
         sub_graph = snapshot['subgraph']
 
         metrics_row = compute_graph_metrics(g=sub_graph)
 
+        average_metrics.append(compute_average_metrics(sub_graph))
+        indices.append(snapshot['title'])
+
         add_sheet_to_xlsx(file_writer=file_writer,
-                          data=metrics_row, title='from ' + snapshot['title'])
+                          data=metrics_row, title='from '+snapshot['title'])
+    
+    columns = ['Degree', 'Betweenness', 'Closeness',
+               'Page Rank', 'Clustering Coefficient', 'Eccentricity', 'Mincut', 'Edge betweenness']
+
+    pd_av_m = pd.DataFrame(
+        data=average_metrics, index=indices, columns=columns)
+
+    # add sheets into excel
+    add_sheet_to_xlsx(file_writer=file_writer, data=pd_av_m,
+                      title='Snapshot Average Metrics', index=True)
+
     print('Computed metrics on snapshots')
     # save file
     save_csv_file(file_writer=file_writer)
@@ -67,9 +90,9 @@ arg_parser.add_argument('-i', '--input', required=True)
 arg_parser.add_argument('-o', '--output', required=True)
 
 arg_parser.add_argument('-s', '--span', default=30, type=int)
-arg_parser.add_argument('-f', '--folder_name', default='analysis')
+
 
 args = arg_parser.parse_args()
 
 filters = args.__dict__
-main(span_days=int(filters['span']), folder_name=filters['folder_name'], input_dir=filters['input'] + '/', output_dir=filters['output'] + '/')
+main(span_days=int(filters['span']), input_dir=filters['input'], output_dir=filters['output'])
