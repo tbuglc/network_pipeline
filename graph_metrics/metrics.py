6f8f5b6d2e4974d91550c89abfbc76c194380336
tbuglc <tbuglc@gmail.com>
2023-06-23 13:24:46 -0400

    added centralization metrics

--- graph_metrics/metrics.py
+++ graph_metrics/metrics.py
@@ -6,14 +6,16 @@ import numpy as np
 import powerlaw as pl
 from utils import global_graph_indices
 
+
 def compute_edge_weight_based_on_edge_number(g):
     weights = []
     for e in g.es:
         src, tgt = e.source, e.target
         count = g.count_multiple([(src, tgt)])
         weights = weights + count
-        
+
     return weights
+
 
 def betweenness(g=Graph, average=False, weights=[]):
     if (average):
@@ -31,6 +33,12 @@ def closeness(g=Graph, average=False, weights=[]):
     if (average):
         return mean(g.closeness(mode='all', normalized=True, weights=weights))
     return g.closeness(mode='all', normalized=True, weights=weights)
+
+
+def harmonic(g=Graph, average=False, weights=[]):
+    if (average):
+        return mean(g.harmonic_centrality(mode='all', normalized=True, weights=weights))
+    return g.harmonic_centrality(mode='all', normalized=True, weights=weights)
 
 
 def mincut(g=Graph, average=False):
@@ -55,9 +63,30 @@ def pagerank(g=Graph, average=False,  weights=[]):
     return g.pagerank(directed=True, weights=weights)
 
 
+def centralization(n, metrics=[], metric_name=''):
+    denominator_factor = None
+
+    if not metric_name:
+        return np.nan
+
+    if metric_name == 'degree':
+        denominator_factor = n**2 - 3*n + 2
+    elif metric_name == 'betweenness':
+        denominator_factor = n**3 - 4*n**2 + 5*n - 2
+    elif metric_name == 'harmonic':
+        denominator_factor = (2*n**2 - 6*n + 4)/(2*n - 3)
+    elif metric_name == 'closeness':
+        denominator_factor = (n**2 - 3*n + 2)/(2*n - 3)
+
+    if not denominator_factor:
+        return np.nan
+
+    return np.sum((np.max(metrics) - np.array(metrics))) / denominator_factor
+
+
 def compute_average_metrics(g=Graph):
-    weights  = compute_edge_weight_based_on_edge_number(g)
-    
+    weights = compute_edge_weight_based_on_edge_number(g)
+
     return [degree(g=g, average=True), betweenness(
         g=g, average=True, weights=weights), closeness(g=g, average=True, weights=weights), pagerank(g=g, average=True, weights=weights), clustering_coefficient(
         g=g, average=True), mean(g.eccentricity()),
@@ -65,14 +94,14 @@ def compute_average_metrics(g=Graph):
 
 
 def compute_graph_metrics(g=Graph):
-    weights  = compute_edge_weight_based_on_edge_number(g)
+    weights = compute_edge_weight_based_on_edge_number(g)
     data = {}
+
     data['Degree'] = degree(g=g)
     data['Betweenness'] = betweenness(g=g, weights=weights)
-    data['Closeness'] = closeness(g=g, weights=weights)
+    data['Harmonic'] = harmonic(g=g, weights=weights)
     data['Page Rank'] = pagerank(g=g, weights=weights)
-    data['Clustering Coefficient'] = clustering_coefficient(g=g)
-
+    data['Local clustering coefficient'] = clustering_coefficient(g=g)
 
     data["Revenu"] = g.vs['revenu']
     data["Age"] = g.vs['age']
@@ -87,25 +116,45 @@ def compute_graph_metrics(g=Graph):
 
     return result
 
+
 def global_graph_properties(g=Graph):
-    x0 = g.vcount()
-    x10 = g.ecount()
-    x1 = g.diameter(directed=True)
-    x2 = g.radius()
-    x3 = g.density()
-    x4 = g.average_path_length(directed=True)
-    x6 = g.reciprocity()
-    x7 = mean(g.eccentricity())
-    
-    weights  = compute_edge_weight_based_on_edge_number(g)
-    
-    x8 = clustering_coefficient(g, average=True)
-    x9 = edge_betweenness(g, average=True, weights=weights)
+    weights = compute_edge_weight_based_on_edge_number(g)
 
+    n = len(g)
 
-    data = [x0, x10,x1, x2, x3, x4,  x6, x7, x8, x9]
+    weak_size, strong_size = giant_component(g)
+
+    data = [
+        g.vcount(),
+        g.ecount(),
+        g.diameter(directed=True),
+        g.radius(),
+        g.density(),
+        g.reciprocity(),
+        mean_degree(g),
+        g.average_path_length(directed=True),
+        weak_size,
+        strong_size,
+        power_law_alpha(g),
+        global_clustering_coefficient(g),
+        clustering_coefficient(g, average=True),
+        centralization(n, degree(g), 'degree'),
+        centralization(n, betweenness(g, weights=weights), 'betweenness'),
+        centralization(n, harmonic(g, weights=weights), 'harmonic'),
+        mean(g.eccentricity()),
+        pagerank(g, average=True, weights=weights),
+        degree_assortativity(g),
+        homophily_nominal(g, 'age'),
+        homophily_nominal(g, 'revenu'),
+        homophily_nominal(g, 'ville'),
+        homophily_nominal(g, 'region'),
+        homophily_nominal(g, 'arrondissement'),
+        homophily_nominal(g, 'adresse'),
+
+    ]
 
     return data
+
 
 def compute_global_properties_on_graph(g=Graph):
     columns = ['Value']
@@ -128,28 +177,36 @@ def compute_degree_distribution(g=Graph, degree_mode='out'):
 def mean_degree(g):
     return g.ecount() / g.vcount()
 
-def small_world_mean_distance(g):
-    shortest_paths_pairs = pd.DataFrame(g.shortest_paths())
-    
-    shortest_paths_pairs.replace([np.inf, -np.inf], 0, inplace=True)
 
-    total_distance = sum(sum(row) for row in shortest_paths_pairs.values)
+'''
+    DEPRECATED: replaced with average_path_lenght from igraph
+'''
+# def small_world_mean_distance(g):
+#     shortest_paths_pairs = pd.DataFrame(g.shortest_paths())
 
-    l = total_distance / g.vcount()**2
-    log_mean_dist = np.log10(g.vcount())
-    print(g.vcount())
-    return l, log_mean_dist  
+#     shortest_paths_pairs.replace([np.inf, -np.inf], 0, inplace=True)
+
+#     total_distance = sum(sum(row) for row in shortest_paths_pairs.values)
+
+#     l = total_distance / g.vcount()**2
+#     log_mean_dist = np.log10(g.vcount())
+#     print(g.vcount())
+#     return l, log_mean_dist
 
 
 def giant_component(g):
-    print(g.vcount(), g.ecount())
+    print(g.vcount(),
+          g.ecount())
     s_components = g.connected_components(mode='strong')
     w_components = g.connected_components(mode='weak')
     # print(s_components)
-    S_s = len(max(s_components, key=len)) / g.vcount() # giant strongly component
-    S_w = len(max(w_components, key=len)) / g.vcount() # giant weakly component
+    s = len(max(s_components, key=len)) / \
+        g.vcount()  # giant strongly component
+    w = len(max(w_components, key=len)) / \
+        g.vcount()  # giant weakly component
 
-    return S_s, S_w
+    return w, s
+
 
 def power_law_alpha(g):
     degrees = g.degree()
@@ -161,12 +218,13 @@ def power_law_alpha(g):
     return alpha
 
 
-def clustering_coefficient(g):
-    l_cc = g.transitivity_undirected(mode="zero")
+def global_clustering_coefficient(g):
+    # l_cc = g.transitivity_undirected(mode="zero")
 
     g_cc = g.transitivity_avglocal_undirected(mode="zero")
 
-    return l_cc, g_cc
+    return g_cc
+
 
 def degree_correlation(g):
 
@@ -186,6 +244,7 @@ def degree_correlation(g):
 
     return d_c
 
+
 def degree_assortativity(g):
     return g.assortativity_degree()
 
@@ -195,12 +254,11 @@ def homophily_nominal(g, attribute):
     if type(g.vs[attribute][0]) == int:
         return g.assortativity_nominal(attribute, directed=True)
 
-
     for v in g.vs:
         # print(v[attribute])
         if type(v[attribute]) == float or type(v[attribute]) == int:
             v[attribute] = 'other'
-        
+
     uni_val = np.unique(g.vs[attribute])
 
     mapping = {}
@@ -210,9 +268,8 @@ def homophily_nominal(g, attribute):
     types = [mapping[att] for att in g.vs[attribute]]
 
     numeric_label = f'{attribute}_numeric_types'
-    
-    g.vs[numeric_label] = types
 
+    g.vs[numeric_label] = types
 
     assortativity = g.assortativity_nominal(numeric_label, directed=True)
 
