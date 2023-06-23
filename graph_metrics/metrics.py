db8309ae94971fd6898f3114ca620305752d36c6
tbuglc <tbuglc@gmail.com>
2023-06-23 13:45:27 -0400

    local changes

--- graph_metrics/metrics.py
+++ graph_metrics/metrics.py
@@ -4,14 +4,16 @@ from igraph import Graph, rescale
 import pandas as pd
 from utils import global_graph_indices
 
+
 def compute_edge_weight_based_on_edge_number(g):
     weights = []
     for e in g.es:
         src, tgt = e.source, e.target
         count = g.count_multiple([(src, tgt)])
         weights = weights + count
-        
+
     return weights
+
 
 def betweenness(g=Graph, average=False, weights=[]):
     if (average):
@@ -54,8 +56,8 @@ def pagerank(g=Graph, average=False,  weights=[]):
 
 
 def compute_average_metrics(g=Graph):
-    weights  = compute_edge_weight_based_on_edge_number(g)
-    
+    weights = compute_edge_weight_based_on_edge_number(g)
+
     return [degree(g=g, average=True), betweenness(
         g=g, average=True, weights=weights), closeness(g=g, average=True, weights=weights), pagerank(g=g, average=True, weights=weights), clustering_coefficient(
         g=g, average=True), mean(g.eccentricity()),
@@ -63,14 +65,13 @@ def compute_average_metrics(g=Graph):
 
 
 def compute_graph_metrics(g=Graph):
-    weights  = compute_edge_weight_based_on_edge_number(g)
+    weights = compute_edge_weight_based_on_edge_number(g)
     data = {}
     data['Degree'] = degree(g=g)
     data['Betweenness'] = betweenness(g=g, weights=weights)
     data['Closeness'] = closeness(g=g, weights=weights)
     data['Page Rank'] = pagerank(g=g, weights=weights)
     data['Clustering Coefficient'] = clustering_coefficient(g=g)
-
 
     data["Revenu"] = g.vs['revenu']
     data["Age"] = g.vs['age']
@@ -85,26 +86,31 @@ def compute_graph_metrics(g=Graph):
 
     return result
 
+
 def global_graph_properties(g=Graph):
-    x0=g.vcount()
-    x10 = g.ecount()
-    x1 = g.diameter(directed=True)
-    x2 = g.radius()
-    x3 = g.density()
-    x4 = g.average_path_length(directed=True)
-    # x5 = g.girth()
-    x6 = g.reciprocity()
-    x7 = mean(g.eccentricity())
-    
-    weights  = compute_edge_weight_based_on_edge_number(g)
-    
+    # x0 =
+    # x10 =
+    # x1 =
+    # x2 =
+    # x3 =
+    # x4 =
+    # # x5 = g.girth()
+    # x6 =
+    # # x7 = mean(g.eccentricity())
+
+    weights = compute_edge_weight_based_on_edge_number(g)
+
+    # TODO: see what to do with these below
     x8 = clustering_coefficient(g, average=True)
     x9 = edge_betweenness(g, average=True, weights=weights)
 
 
-    data = [x0, x10,x1, x2, x3, x4,  x6, x7, x8, x9]
+
+    data = [g.vcount(), g.ecount(), g.diameter(directed=True), g.radius(), g.density(
+    ), g.average_path_length(directed=True),  g.reciprocity(), ]
 
     return data
+
 
 def compute_global_properties_on_graph(g=Graph):
     columns = ['Value']
